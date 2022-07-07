cf2e5012aee7d46db8f01773ae10c2b7453388dd
timurkazan99 <timurkazan99@gmail.com>
2022-07-08 01:14:11 +0300

    add clean webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -11,7 +12,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-        })
+        }),
+        new CleanWebpackPlugin(),
     ],
     module: {
         rules: [
