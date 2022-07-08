b0057a677ebcabbf8618b6d8aaa83dfa170a9c98
timurkazan99 <timurkazan99@gmail.com>
2022-07-08 14:37:39 +0300

    add locales

---
+++ src/locales/en.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    containers: {
+      postsContainer_title: 'Posts',
+      feedsContainer_title: 'Feeds',
+    },
+    feedbacks: {
+      non_valid_rss: 'The resource does not contain valid RSS',
+      doubles_alert: 'RSS already exists',
+      invalid_url: 'Link must be a valid URL',
+      upload_success: 'RSS uploaded successfully',
+      network_error: 'Network error',
+    },
+    buttons: {
+      description_window: 'view',
+      open_article: 'read full article',
+      close_article: 'close',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    containers: {
+      postsContainer_title: 'Посты',
+      feedsContainer_title: 'Фиды',
+    },
+    feedbacks: {
+      non_valid_rss: 'Ресурс не содержит валидный RSS',
+      doubles_alert: 'RSS уже существует',
+      invalid_url: 'Ссылка должна быть валидным URL',
+      upload_success: 'RSS успешно загружен',
+      network_error: 'Ошибка сети',
+    },
+    buttons: {
+      description_window: 'Просмотр',
+      open_article: 'Читать полностью ',
+      close_article: 'Закрыть',
+    },
+  },
+};
